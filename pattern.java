b28287e1ee5f8ecb0940184362a2c49682fe7047
aj1511 <58018867+aj1511@users.noreply.github.com>
2020-10-01 10:33:37 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class pattern {
+	public static void main(String[] args) {
+	
+	int x,n,i=6,y=1;
+	for (x=1;x<i;x++)
+	{
+		for(n=1;n<=x;n++) {
+			System.out.print(y+" ");
+			y++;}
+		System.out.println();
+	}
+	for(x=i;x>=1;x--)
+	{
+		for(n=x;n>=1;n--) 
+		
+			{
+			System.out.print(y+" ");
+			y++;}
+		System.out.println();
+	}
+	}
+}
